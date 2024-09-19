5306664e75608facd26d1ce2a59555403a0bd88d
Dayvson Silva <166171637+Dayvson-Silva@users.noreply.github.com>
2024-09-19 17:50:03 -0300

    Update script.js
    mudou para 3 imagens o carrosel

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ const slides = document.querySelector('.slides');
 // Obtém o número total de slides (imagens) dentro do container .slides
 const totalSlides = slides.children.length;
 
+// Define a largura de cada slide
+const slideWidth = 2000; // Largura de cada slide em pixels
+const slidesToShow = 3; // Número de slides a serem exibidos de uma vez
+
 // Função para mostrar o próximo slide
 function showNextSlide() {
     // Incrementa o índice currentIndex e garante que ele volte para 0 ao atingir o número total de slides
-    currentIndex = (currentIndex + 1) % totalSlides;
+    currentIndex = (currentIndex + 1) % (totalSlides - slidesToShow + 1);
 
     // Move o container de slides para a esquerda, baseando-se no índice atual
-    slides.style.transform = `translateX(-${currentIndex * 2000}px)`; // Corrige para mover 2000px por slide
+    slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`; // Mova slideWidth por índice
 }
 
 // Chama a função showNextSlide a cada 3 segundos (3000 milissegundos)
